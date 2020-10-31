69da878c58e62dc5ef8580da137ae74d00eea016
cod4adm <cod4adm@gmail.com>
2020-10-31 18:57:58 +0200

    final version

---
+++ src/app/dialog/edit-category-dialog/edit-category-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-category-dialog',
+  templateUrl: './edit-category-dialog.component.html',
+  styleUrls: ['./edit-category-dialog.component.css']
+})
+export class EditCategoryDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/dialog/edit-priority-dialog/edit-priority-dialog.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-priority-dialog',
+  templateUrl: './edit-priority-dialog.component.html',
+  styleUrls: ['./edit-priority-dialog.component.css']
+})
+export class EditPriorityDialogComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
